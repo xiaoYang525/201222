dcc31ea76b4614975e8c7bdbce6ab336026bc972
xiaoYang <2719496330@qq.com>
2020-12-24 23:23:55 +0800

    day2

---
+++ day01/3/pages/Search/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Search组件</h2>
+    <h2>route.params.keyword:{{keyword}}</h2>
+    <h2>route.query.content:{{content}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+ props:['keyword','content']
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
